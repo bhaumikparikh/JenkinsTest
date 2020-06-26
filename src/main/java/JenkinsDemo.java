4211c2f2ff67462a69dd74ba66d49fb91f116621
bhaumik <xs2bhaumik@gmail.com>
2020-06-25 23:15:45 -0400

    First Git Commit

---
+++ src/main/java/JenkinsDemo.java
@@ -0,0 +1,12 @@
+public class JenkinsDemo {
+    public static void main(String[] args) {
+
+        String Name = "Babayega";
+        String Code = "Boogyman";
+        System.out.println(Name);
+        System.out.println(Code);
+
+    }
+
+}
+
